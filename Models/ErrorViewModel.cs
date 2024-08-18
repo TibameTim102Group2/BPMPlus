0df74bbd26bf48761bedb5662bc6134f9806b99e
RupertPracticeProject <taiwanourland@gmail.com>
2024-08-18 17:34:05 +0800

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BPMPlus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
