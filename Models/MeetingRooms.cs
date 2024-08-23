b327fe799e043ed89d92c88003644d911f771c79
Jamie <jamietu08@gmail.com>
2024-08-23 19:02:22 +0800

    Add Function, Grade, MeetingRoom and Group

---
+++ Models/MeetingRooms.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BPMPlus.Models
+{
+    public class MeetingRooms
+    {
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(20)]
+        [Key]
+        public string MeetingRoom { get; set; }
+        public int Accomadation { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+
+
+    }
+}
