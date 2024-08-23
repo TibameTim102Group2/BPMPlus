bc1d58ed7fa60d375ba96a326e059c4beb2a282e
RupertPracticeProject <taiwanourland@gmail.com>
2024-08-23 13:58:26 +0800

    Add base table

---
+++ Controllers/MeetingsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BPMPlus.Data;
+using BPMPlus.Models;
+
+namespace BPMPlus.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MeetingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Meetings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Meeting.ToListAsync());
+        }
+
+        // GET: Meetings/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meeting
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // GET: Meetings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Meetings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MeetingId,MeetingRoom,StartTime,EndTime,Note,CreatedTime,UpdateTime")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(meeting);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meeting);
+        }
+
+        // GET: Meetings/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meeting.FindAsync(id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+            return View(meeting);
+        }
+
+        // POST: Meetings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MeetingId,MeetingRoom,StartTime,EndTime,Note,CreatedTime,UpdateTime")] Meeting meeting)
+        {
+            if (id != meeting.MeetingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meeting);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MeetingExists(meeting.MeetingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meeting);
+        }
+
+        // GET: Meetings/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meeting
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // POST: Meetings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var meeting = await _context.Meeting.FindAsync(id);
+            if (meeting != null)
+            {
+                _context.Meeting.Remove(meeting);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MeetingExists(string id)
+        {
+            return _context.Meeting.Any(e => e.MeetingId == id);
+        }
+    }
+}
---
+++ Models/Meeting.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Azure;
+
+namespace BPMPlus.Models
+{
+    public class Meeting
+    {
+        public List<User> Users{ get; } = [];
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(20)]
+        public string MeetingId { get; set; }
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(20)]
+        public string MeetingRoom { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? Note { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}
