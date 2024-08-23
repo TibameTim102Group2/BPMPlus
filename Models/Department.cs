d56abe23b17c73ab0424c0dc7e01ee0e7ae526ba
Ian <jacky8329431@gmail.com>
2024-08-23 19:07:27 +0800

    add table

---
+++ Models/Department.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BPMPlus.Models
+{
+    public class Department
+    {
+        [Key]
+        [Column(TypeName = "varchar")]
+        [MaxLength(20)]
+        public string DepartmentId { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(20)]
+        public string DepartmentName { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedTime { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime UpdateTime { get; set; }
+
+    }
+}
---
+++ Models/ProcessTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BPMPlus.Models
+{
+    public class ProcessTemplate
+    {
+        [Key]
+        [Column(TypeName = "varchar")]
+        [MaxLength(20)]
+        public string ProcessNodeId { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(20)]
+        public string FunctionId { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(20)]
+        public string CategoryId { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedTime { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime UpdateTime { get; set; }
+    }
+}
