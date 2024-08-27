6d2432a58cf4a60670c20bc7fb8e826acc8ec93a
RupertPracticeProject <taiwanourland@gmail.com>
2024-08-27 23:40:11 +0800

    form detail and modify form

---
+++ Controllers/FormDetails.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.Controllers
+{
+    public class FormDetails : Controller
+    {
+        // GET: FormDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/ModifyForm.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.Controllers
+{
+    public class ModifyForm : Controller
+    {
+        // GET: ModifyForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
