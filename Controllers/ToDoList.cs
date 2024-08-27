93466356a125d2888f125c7e2498e6d5a59201ea
RupertPracticeProject <taiwanourland@gmail.com>
2024-08-27 19:41:30 +0800

    增加_Layout.cshtml 導覽列、標題連結

---
+++ Controllers/ToDoList.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.Controllers
+{
+    public class ToDoList : Controller
+    {
+        // GET: ToDoList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ToDoList/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ToDoList/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDoList/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ToDoList/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ToDoList/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ToDoList/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ToDoList/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
