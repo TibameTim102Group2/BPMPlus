c888ebb82db86813e2f310b5204c4db92864cf3d
Ian <jacky8329431@gmail.com>
2024-08-30 23:09:43 +0800

    Add LoginController, LoginView*4

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.Controllers
+{
+	public class LoginController : Controller
+	{
+		// GET: LoginController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+        public ActionResult EmailValid()
+        {
+            return View();
+        }
+
+        public ActionResult EmailForgetPassWord()
+        {
+            return View();
+        }
+        public ActionResult ResetPassWord()
+        {
+            return View();
+        }
+    }
+}
