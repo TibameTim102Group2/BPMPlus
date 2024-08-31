226173ffaaf7edf43b11650fb48bd9b742cca0a3
RupertPracticeProject <taiwanourland@gmail.com>
2024-08-31 18:06:41 +0800

    修管線

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace BPMPlus
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
