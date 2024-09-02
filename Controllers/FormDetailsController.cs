65cfada6a5d6e9661d95fd431396a5c9eca18e86
Jamie <jamietu08@gmail.com>
2024-09-02 12:01:19 +0800

    Add viewmodels,update FormDetails

--- Controllers/FormDetailsController.cs
+++ Controllers/FormDetailsController.cs
@@ -1,15 +1,71 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BPMPlus.Data;
+using BPMPlus.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BPMPlus.Controllers
 {
+    
     public class FormDetailsController : Controller
     {
-        // GET: FormDetails
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public FormDetailsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        // GET: FormDetails
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        //[HttpPost]
+        // GET: FormDetails
+        //public IActionResult Index(string id)
+        //{
+        //    var result = _context.Form.Include(c => c.ProcessNode).ThenInclude(c => c.UserActivity).Include(c => c.FormRecord).
+        //        Include(c => c.Project).Include(c => c.Category).Include(c => c.Department).Include(c => c.User)
+        //        .Where(c => c.FormId == id)
+        //        .Select(c => new FormDetailsViewModel
+        //        {
+        //            FormId = c.FormId,
+        //            ProjectName = c.Project.ProjectName,
+        //            CategoryDescription = c.Category.CategoryDescription,
+        //            Tel = c.Tel,
+        //            ExpectedFinishedDay = c.ExpectedFinishedDay,
+        //            Content = c.Content,
+        //            CreatedTime = c.CreatedTime,
+        //            DepartmentName = c.Department.DepartmentName,
+        //            EmployeeName = c.User.UserName,
+        //            FormDetailsProcessNodes = c.ProcessNode.Select(d => new FormDetailsProcessNodeViewModel
+        //            {
+        //                UserActivityIdDescription = d.UserActivity.UserActivityIdDescription,
+        //                IsHightLight = true,
+        //            }).ToList(),
+        //            FormDetailsFormProcesses = c.FormRecord.Select(d => new FormDetailsFormProcessViewModel
+        //            {
+        //                CreatedTime = d.CreatedTime,
+        //                UserActivityIdDescription = d.UserActivity.UserActivityIdDescription,
+        //                EmployeeName = d.User.UserName,
+        //                Remark = d.Remark,
+        //                ResultDescription = d.Result.ResultDescription
+        //            }).ToList()
+        //        }).FirstOrDefault();
+
+        //    return View(result);
+        //}
+
+
+        // GET: FormDetails
+        public IActionResult Index()
+        {
+            return View(_context.Form.FirstOrDefault());
+        }
+
 
     }
 }
---
+++ ViewModels/FormDetailsFormProcessViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace BPMPlus.ViewModels
+{
+    public class FormDetailsFormProcessViewModel
+    {
+        public DateTime? CreatedTime { get; set; }
+
+        public string UserActivityIdDescription { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Remark { get; set; }
+
+        public string ResultDescription { get; set; }
+    }
+}
