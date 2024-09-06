4a1d3f84d08e161c07303eaa31dabd6bdf827888
Jamie <jamietu08@gmail.com>
2024-09-06 10:20:16 +0800

    Update ViewModel&Controller&View:FormDetails/ModifyForm

---
+++ ViewModels/FormBaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace BPMPlus.ViewModels
+{
+    public class FormBaseViewModel
+    {
+        public string FormId { get; set; }
+
+        public string Id { get; set; }
+
+        public string Date { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string ExpectedFinishedDay { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public string Content { get; set; }
+
+        public string Tel { get; set; }
+
+        public bool FormIsActive { get; set; }
+
+    }
+}
---
+++ ViewModels/ModifyFormGroupViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BPMPlus.ViewModels
+{
+    public class ModifyFormGroupViewModel
+    {
+
+        public ModifyFormViewModel ModifyFrom { get; set; }
+        public IList<ModifyFormCategoryViewModel> Categories { get; set; }
+        public IList<ModifyFormProjectViewModel> Projects { get; set; }
+    }
+}
