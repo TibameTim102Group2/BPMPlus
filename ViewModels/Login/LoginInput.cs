4ba6709485c10931808ea37bb98cc93dabe4bad7
Risa <risa.00112279@gmail.com>
2024-09-10 19:21:36 +0800

    使用者密碼加入BCrypt加密 (pull後請重加DB)

---
+++ ViewModels/Login/LoginInput.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.ViewModels.Login
+{
+    public class LoginInput
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
