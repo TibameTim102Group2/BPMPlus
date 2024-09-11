c5a01761f7d176e0f1a83aeba19b4ceda749cb18
Jamie <jamietu08@gmail.com>
2024-09-11 21:31:05 +0800

    ModifyForm Index修改

---
+++ Controllers/CreateProjectsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BPMPlus.Data;
+using BPMPlus.Models;
+
+namespace BPMPlus.Controllers
+{
+    public class CreateProjectsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateProjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CreateProjects
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+     
+    }
+}
