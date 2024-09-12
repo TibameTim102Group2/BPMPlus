8b3ca8df2b48234ae5c92f494456f95cf733ba5e
Bob <bobworld1039@gmail.com>
2024-09-12 11:47:49 +0800

    background Users query details function and add relation tables

---
+++ Data/Migrations/20240912034334_Add PermissionGroupUser&PermissionGroupUserActivity relation tables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BPMPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissionGroupUserPermissionGroupUserActivityrelationtables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
