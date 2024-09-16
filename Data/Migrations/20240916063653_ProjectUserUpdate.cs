79b96ff7c79d39e7b11a434da9ffe9540216dcbe
Jamie <jamietu08@gmail.com>
2024-09-16 14:49:15 +0800

    更新projectUser

---
+++ Data/Migrations/20240916063653_ProjectUserUpdate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BPMPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectUserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectManagerId",
+                table: "Project",
+                type: "VARCHAR(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectManagerId",
+                table: "Project");
+        }
+    }
+}
