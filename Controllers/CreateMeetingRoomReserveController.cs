c34dfac02dfe43687f6bcb1dc7057ace1c1c2add
Risa <risa.00112279@gmail.com>
2024-09-18 17:19:38 +0800

    add meetingroom controller.views

---
+++ Controllers/CreateMeetingRoomReserveController.cs
@@ -0,0 +1,20 @@
+﻿using BPMPlus.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.Controllers
+{
+    public class CreateMeetingRoomReserveController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateMeetingRoomReserveController(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/CurrentMeetingRoomController.cs
@@ -0,0 +1,20 @@
+﻿using BPMPlus.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPMPlus.Controllers
+{
+    public class CurrentMeetingRoomController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CurrentMeetingRoomController(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
