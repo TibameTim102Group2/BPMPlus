28344e2f38f6efc6f9b9a7e636ff7e5801806366
Risa <risa.00112279@gmail.com>
2024-09-24 20:57:27 +0800

    會議室預約表格done

--- Controllers/BookingMeetingRoomController.cs
+++ Controllers/BookingMeetingRoomController.cs
@@ -25,6 +25,11 @@ namespace BPMPlus.Controllers
         {
             User user = await GetAuthorizedUser();
 
+            var rooms = await _context.MeetingRooms.Select(r => r.MeetingRoomId).ToArrayAsync();
+            var timeSlots = GetTimeSlots(8, 22, 60);
+            ViewBag.Rooms = rooms;
+            ViewBag.TimeSlots = timeSlots;
+
             //預約人部門
             var Department = await _context.Department
                  .FirstOrDefaultAsync(d => d.DepartmentId == user.DepartmentId);
@@ -51,6 +56,22 @@ namespace BPMPlus.Controllers
             return View();
         }
 
+        
+        private List<DateTime> GetTimeSlots(int start, int end, int Minutes)
+        {
+            var slots = new List<DateTime>();
+            var currentTime = DateTime.Today.AddHours(start);
+            var endTime = DateTime.Today.AddHours(end);
+
+            while (currentTime <= endTime)
+            {
+                slots.Add(currentTime);
+                currentTime = currentTime.AddMinutes(Minutes);
+            }
+
+            return slots;
+        }
+
         // 撈會議室可容納人數
         public async Task<IActionResult> GetMeetingRoomInfo(string id)
         {
@@ -58,14 +79,14 @@ namespace BPMPlus.Controllers
                 .Where(n => n.MeetingRoomId == id)
                 .Select(n => n.Accommodation);
 
-            return Json(new { success=true, data=accommdation });
+            return Json(new { success=true, data = accommdation });
         }
 
         //GetBookedTime
-        public async Task<IActionResult> CheakMeetingRooms(string RoomId, string BookingDate)
+        public async Task<IActionResult> CheakMeetingRooms(string BookingDate)
         {
             var bookedTimes = await _context.Meeting
-                .Where(b=>b.MeetingRoomId == RoomId && b.StartTime.Date == DateTime.Parse(BookingDate).Date)
+                .Where(b=>b.StartTime.Date == DateTime.Parse(BookingDate).Date)
                 .Select(b=>new {
                     StartTime = b.StartTime.AddHours(8).ToString("HH:mm"),
                     EndTime = b.EndTime.AddHours(8).ToString("HH:mm"),
@@ -75,25 +96,46 @@ namespace BPMPlus.Controllers
             return Json(new { success = true, data = bookedTimes });
         }
 
+
         [HttpPost]
         public async Task<IActionResult> SubmitBooking([FromBody] BookingMeetingRoomVM vm)
         {
+            var bookedTime = await _context.Meeting
+                .Where(d => d.StartTime.Date == DateTime.Parse(vm.Date) && d.MeetingRoomId == vm.Room)
+                .Select(d => new { d.StartTime, d.EndTime })
+                .ToListAsync();
 
-            List<string> id = await CreateMeetingIdListAsync(1);
+
+            string[] start = new string[14];
+            string[] end = new string[14];
+            foreach (var time in bookedTime)
+            {
+
+            }
+
+
 
             if (ModelState.IsValid)
             {
+                var StartTime = DateTime.Parse($"{vm.Date} {vm.StartTime}").AddHours(-8);
+                var EndTime = DateTime.Parse($"{vm.Date} {vm.EndTime}").AddHours(-8);
+
+                
+
+
                 Meeting meeting = new Meeting();
+                List<string> id = await CreateMeetingIdListAsync(1);
 
                 var host = await _context.User
                     .Where(u => u.UserName == vm.MeetingHost)
                     .Select(h => h.UserId)
                     .FirstOrDefaultAsync();
 
+
                 meeting.MeetingId = id.FirstOrDefault();
                 meeting.MeetingRoomId = vm.Room;
-                meeting.StartTime = DateTime.ParseExact(vm.StartDate, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture).AddHours(-8);
-                meeting.EndTime = DateTime.ParseExact(vm.EndDate, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture).AddHours(-8);
+                meeting.StartTime = StartTime;
+                meeting.EndTime = EndTime;
                 meeting.MeetingHost = host;
                 meeting.Note = vm.Note;
                 meeting.CreatedTime = DateTime.Now.AddHours(-8);
@@ -101,20 +143,27 @@ namespace BPMPlus.Controllers
 
                 await _context.Meeting.AddAsync(meeting);
                 await _context.SaveChangesAsync();
-            }
-            var meetingid = _context.Meeting
-                .Include(x => x.Users)
-                .FirstOrDefault(x => x.MeetingId == id[0]);
 
-            foreach (var memberId in vm.Members)
-            {
-                //重新建關聯
-                var newMeetingMember = _context.User.FirstOrDefault(pg => pg.UserId == memberId);
-                if (newMeetingMember != null)
+
+                var meetingid = _context.Meeting
+                    .Include(x => x.Users)
+                    .FirstOrDefault(x => x.MeetingId == id[0]);
+
+                foreach (var memberId in vm.Members)
                 {
-                    meetingid.Users.Add(newMeetingMember);
-                    _context.SaveChanges();
+                    //重新建關聯
+                    var newMeetingMember = _context.User.FirstOrDefault(pg => pg.UserId == memberId);
+                    if (newMeetingMember != null)
+                    {
+                        meetingid.Users.Add(newMeetingMember);
+                        _context.SaveChanges();
+                    }
                 }
+            }
+            else
+            {
+                var falseText = "預約失敗! 請重新預約!";
+                return Json(new { success = false, data = falseText });
             }
             return RedirectToAction("Index", "CurrentMeetingRoom");
         }
